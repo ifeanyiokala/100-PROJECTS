1f8687c7dd6007589faa1954d60e6accf34dc439
ifeanyiokala <ifeanyi.okala.247238@unn.edu.ng>
2022-02-22 21:12:28 +0100

    100 Python Projects in 100 days

---
+++ DAY 17.py
@@ -0,0 +1,9 @@
+class Question : 
+    
+    def __init__(self,text,answer):
+        self.txt = text 
+        self.ans = answer 
+
+question_1 = Question("What's your name","Ifeanyi")
+
+print(question_1.txt)
