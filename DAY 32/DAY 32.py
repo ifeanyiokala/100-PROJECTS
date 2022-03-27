564612129c7660842d2348fb7e0addeae53274f6
ifeanyiokala <ifeanyi.okala.247238@unn.edu.ng>
2022-03-27 23:30:23 +0100

    100 Python Projects in 100 Days

---
+++ DAY 32/DAY 32.py
@@ -0,0 +1,35 @@
+# import needed modules
+from datetime import datetime
+import smtplib
+import pandas as pd
+import random
+
+
+my_email = "bbforlife998@yahoo.com"
+password_1 = "badboysforlife"
+# Assigning the month and day to a variable 
+today = datetime.now()
+today_tuple = (today.month, today.day)
+
+
+# Use pandas to read the csv file
+data =  pd.read_csv("birthdays.csv")
+
+# Use dictionary comprehension to create a dictionary from the csv file 
+birthdays_dict = {(data_row["month"], data_row["day"]): data_row for (index, data_row) in data.iterrows()}
+print(birthdays_dict)
+if today_tuple in birthdays_dict:
+    birthday_person = birthdays_dict[today_tuple]
+    file_path = f"letter_templates/letter_{random.randint(1,3)}.txt"
+    with open(file_path) as letter_file:
+        contents = letter_file.read()
+        contents = contents.replace("[NAME]", birthday_person["name"])
+
+    with smtplib.SMTP('smtp.mail.yahoo.com') as connection:
+        connection.starttls()
+        connection.login(my_email, password_1)
+        connection.sendemail(
+                from_addr=my_email, 
+                to_addrs=birthday_person["email"], 
+                msg=f"Subject:Happy Birthday\n\n{contents}"
+)
